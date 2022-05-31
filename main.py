4bef9e512b2b56e21cc209a84c1406d403412bea
Adolfo Matias <petadolfo@gmail.com>
2022-05-30 23:51:21 -0300

    Etapa 1: primeiro commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from copyreg import pickle
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+@app.route("/index", methods=["POST"])
+def predicao():
+    salario = float(request.form.get("sal"))
+    idade = float(request.form.get("ida"))
+    divida=float(request.form.get("divi"))
+    modelo = pickle.load(open("./model.pkl", "rb"))
+    prever = modelo.predict([[salario, idade,divida]])
+    return (f"Credito: {str(prever)}")
+
+if __name__ == "__main__":
+    app.run(port=5000)
+
